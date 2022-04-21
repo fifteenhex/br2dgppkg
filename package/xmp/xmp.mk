b9573860f4576c3360ec02089661fc7f88843882
Daniel Palmer <daniel@0x0f.com>
2022-04-21 21:35:25 +0900

    pkg: xmp: Use ALSA if available

--- package/xmp/xmp.mk
+++ package/xmp/xmp.mk
@@ -11,6 +11,12 @@ XMP_LICENSE = GPL-2.0
 XMP_LICENSE_FILES = COPYING
 XMP_AUTORECONF = YES
 XMP_DEPENDENCIES = host-pkgconf libxmp
+XMP_CONF_OPTS = --disable-oss
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+XMP_DEPENDENCIES += alsa-lib
+XMP_CONF_OPTS += --enable-alsa
+endif
+
 
 $(eval $(autotools-package))
-
